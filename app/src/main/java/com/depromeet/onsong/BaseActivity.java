7d912537c8d2f1f60012fe8f76cc9e7bf1038c41
gracefulife <gracefulife@naver.com>
2018-06-27 23:04:23 +0900

    베이스 액티비티 추가

--- app/src/main/java/com/depromeet/onsong/BaseActivity.java
+++ app/src/main/java/com/depromeet/onsong/BaseActivity.java
@@ -1,4 +1,30 @@
 package com.depromeet.onsong;
 
-public class BaseActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import io.reactivex.disposables.CompositeDisposable;
+
+public abstract class BaseActivity extends AppCompatActivity {
+  protected CompositeDisposable compositeDisposable;
+
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    compositeDisposable = new CompositeDisposable();
+  }
+
+  @Override protected void onDestroy() {
+    super.onDestroy();
+    if (compositeDisposable != null) {
+      compositeDisposable.dispose();
+    }
+  }
+
+  @Override public void setContentView(View view) {
+    super.setContentView(view);
+    ButterKnife.bind(this);
+  }
 }
