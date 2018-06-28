524c04e59b43d436e864ac781f7694be29d20349
gracefulife <gracefulife@naver.com>
2018-06-28 23:46:52 +0900

    optimize imports

--- app/src/test/java/com/depromeet/onsong/GenreStoreTest.java
+++ app/src/test/java/com/depromeet/onsong/GenreStoreTest.java
@@ -25,7 +25,6 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
 
 public class GenreStoreTest {
 
