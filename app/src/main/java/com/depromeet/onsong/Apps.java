c5a04e542b4201f01dba77f3e09b0f43abb367ea
gracefulife <gracefulife@naver.com>
2018-06-28 22:27:30 +0900

    켈리그라피 설정

--- app/src/main/java/com/depromeet/onsong/Apps.java
+++ app/src/main/java/com/depromeet/onsong/Apps.java
@@ -2,6 +2,10 @@ package com.depromeet.onsong;
 
 import android.app.Application;
 
+import io.github.inflationx.calligraphy3.CalligraphyConfig;
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
+import io.github.inflationx.viewpump.ViewPump;
+
 public class Apps extends Application {
   private static Apps instance;
 
@@ -9,6 +13,14 @@ public class Apps extends Application {
     super.onCreate();
 
     instance = this;
+
+    ViewPump.init(ViewPump.builder()
+        .addInterceptor(new CalligraphyInterceptor(
+            new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/NotoSansKR-Light-Hestia.otf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()))
+        .build());
   }
 
   public static Apps get() {
--- app/src/main/java/com/depromeet/onsong/BaseActivity.java
+++ app/src/main/java/com/depromeet/onsong/BaseActivity.java
@@ -1,10 +1,12 @@
 package com.depromeet.onsong;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import butterknife.ButterKnife;
+import io.github.inflationx.viewpump.ViewPumpContextWrapper;
 import io.reactivex.disposables.CompositeDisposable;
 
 public abstract class BaseActivity extends AppCompatActivity {
@@ -25,5 +27,9 @@ public abstract class BaseActivity extends AppCompatActivity {
   @Override public void setContentView(int layoutResID) {
     super.setContentView(layoutResID);
     ButterKnife.bind(this);
+  }
+
+  @Override protected void attachBaseContext(Context newBase) {
+    super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
   }
 }
