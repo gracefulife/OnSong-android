ade2f31740a744c0a1ac58b4ea9d97f393907dc1
gracefulife <gracefulife@naver.com>
2018-07-05 18:57:13 +0900

    스테이트 셋업

---
+++ app/src/main/java/com/depromeet/onsong/detail/PlayingState.java
@@ -0,0 +1,25 @@
+package com.depromeet.onsong.detail;
+
+import com.depromeet.onsong.playlist.Music;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Grox + 불변 데이터 형식을 사용하므로,
+ * 데이터를 외부에 공개해도 상관없다.
+ */
+@ToString
+@AllArgsConstructor
+public class PlayingState {
+  public final Music music;
+  public final boolean isPlaying;
+  public final boolean isLike;
+  public final String scrollBy;
+
+//  public static PlayingState choose(List<Music> musics, int chosen, String scrollBy) {
+//    return new PlayingState(musics, chosen, scrollBy);
+//  }
+}
