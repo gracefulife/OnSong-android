0b54d77b0f102ab3601a42cf89ee7e2571c17698
gracefulife <gracefulife@naver.com>
2018-07-11 23:41:21 +0900

    네비게이션 기본 UI

--- app/src/main/java/com/depromeet/onsong/home/HomeActivity.java
+++ app/src/main/java/com/depromeet/onsong/home/HomeActivity.java
@@ -254,17 +254,11 @@ public class HomeActivity extends BaseActivity
     // Handle navigation view item clicks here.
     int id = item.getItemId();
 
-    if (id == R.id.nav_camera) {
+    if (id == R.id.nav_bell) {
       // Handle the camera action
-    } else if (id == R.id.nav_gallery) {
+    } else if (id == R.id.nav_hash) {
 
-    } else if (id == R.id.nav_slideshow) {
-
-    } else if (id == R.id.nav_manage) {
-
-    } else if (id == R.id.nav_share) {
-
-    } else if (id == R.id.nav_send) {
+    } else if (id == R.id.nav_settings) {
 
     }
 
